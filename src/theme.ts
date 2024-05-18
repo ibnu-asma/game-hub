56accb1116427527ca006a35b782adec141e8343
Abdulhamid Hayredin <jerryhayat59@gmail.com>
2024-05-18 06:33:11 +0300

    Impliment dark mode

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react';
+
+
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+}
+
+// 3. extend the theme
+const theme = extendTheme({ config })
+
+export default theme
