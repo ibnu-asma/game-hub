f5a286e9aa97593f90f14f8dfa29b88cedc62694
Abdulhamid Hayredin <jerryhayat59@gmail.com>
2024-05-25 11:29:46 +0300

    build game trailer

--- src/Pages/GameDetailPage.tsx
+++ src/Pages/GameDetailPage.tsx
@@ -1,10 +1,10 @@
-import { generatePath, useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import { GridItem, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { Heading, Spinner } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
 import { ExpandableText } from "../components/ExpandableText";
-import { DefinitionItem } from "../components/DefinitionItem";
-import { CriticScore } from "../components/CriticScore";
+import useGame from "../hooks/useGame";
+
 import { GameAttributes } from "../components/GameAttributes";
+import { GameTrailer } from "../components/GameTrailer";
 
 export const GameDetailPage = () => {
   const { slug } = useParams();
@@ -19,6 +19,7 @@ export const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id} />
       
     </>
   );
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,33 +1,41 @@
 import axios, { AxiosRequestConfig } from "axios";
 export interface FetchResponse<T> {
-    next: string | null,
-    count: number,
-    results : T[]
+  next: string | null;
+  count: number;
+  results: T[];
 }
 
-const axiosInstance =  axios.create({
-    baseURL: 'https://api.rawg.io/api/',
-    params : {
-        key: "c653f58d7bf64a89b4070683dec153ea"
-    }
-  });
+const axiosInstance = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "c653f58d7bf64a89b4070683dec153ea",
+  },
+});
 
 class APIClient<T> {
-    endpoint: string;
-    constructor(endpoint: string) {
-        this.endpoint = endpoint;
-    }
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+    console.log(endpoint);
+  }
 
-    getAll = (config: AxiosRequestConfig) => {
-       return axiosInstance.get<FetchResponse<T>>(this.endpoint,config)
-           .then((res)=> res.data);
-    }
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
 
-    get = (id: string | number) => {
-        return axiosInstance.get<T>(this.endpoint + '/' + id).then(res => res.data);
-    }
+  get = (id: string | number) => {
+    return axiosInstance
+      .get<T>(this.endpoint + "/" + id)
+      .then((res) => res.data);
+  };
+
+  getTrailer = (id: number) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint + "/" + id + "/movies")
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
-
-  
